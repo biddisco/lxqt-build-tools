9597ddc6706d6999e39fba706cdd47648d2a7c3f
John Biddiscombe <biddisco@cscs.ch>
2021-12-05 20:43:36 +0100

    Tweak backup script to save bandwidth (git files)

--- sync.sh
+++ sync.sh
@@ -1,30 +1,33 @@
+#params="--delete"
+params="--exclude '*.pack'"
 
 echo "-----------------------------------"
 echo "Updating to Raspberry Pi"
 echo "-----------------------------------"
-$HOME/update-rsync.sh $HOME/src/xrp/grox/ pi@192.168.1.15:/home/pi/src/grox --delete
+$HOME/update-rsync.sh $HOME/src/xrp/grox/ pi@192.168.1.15:/home/pi/src/grox $params
 
 echo 
 echo "-----------------------------------"
 echo "Updating to old laptop (cable)"
 echo "-----------------------------------"
 # RJ45 network cable
-$HOME/update-rsync.sh $HOME/src/xrp/grox/ biddisco@192.168.1.165:/home/biddisco/src/grox --delete
+$HOME/update-rsync.sh $HOME/src/xrp/grox/ biddisco@192.168.1.165:/home/biddisco/src/grox $params
 
 echo 
 echo "-----------------------------------"
 echo "Updating to old laptop (wifi)"
 echo "-----------------------------------"
 # Wifi connection
-$HOME/update-rsync.sh $HOME/src/xrp/grox/ biddisco@192.168.1.112:/home/biddisco/src/grox --delete
+$HOME/update-rsync.sh $HOME/src/xrp/grox/ biddisco@192.168.1.112:/home/biddisco/src/grox $params
 
 echo 
 echo "-----------------------------------  "
 echo "Updating to old laptop (cscs network)"
 echo "-----------------------------------  "
 # Wifi connection
-$HOME/update-rsync.sh $HOME/src/xrp/grox/ biddisco@148.187.133.20:/home/biddisco/src/grox --delete
+$HOME/update-rsync.sh $HOME/src/xrp/grox/ biddisco@148.187.133.28:/home/biddisco/src/grox $params
 
 # to copy the settings
-# scp /home/biddisco/.config/grox.ini 148.187.133.20:/home/biddisco/.config/grox.ini
-# scp /home/biddisco/.ssh/.gkey.sh    148.187.133.20:/home/biddisco/.ssh/.gkey.sh
+# scp /home/biddisco/.config/grox.ini 148.187.133.28:/home/biddisco/.config/grox.ini
+# scp /home/biddisco/.ssh/.gkey.sh    148.187.133.28:/home/biddisco/.ssh/.gkey.sh
+# scp /home/biddisco/.local/share/grox/grox.hdf5 148.187.133.28:/home/biddisco/.local/share/grox/grox.hdf5
