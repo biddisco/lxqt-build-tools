6f374bb96ac3f93dbd222ac1a47e62c9d8c1d1b8
John Biddiscombe <biddisco@cscs.ch>
2024-01-24 16:29:00 +0100

    Small cleanup of qt mainthread (mainwindow) scheduler

--- src/senders/qt_mainthread_scheduler.hpp
+++ src/senders/qt_mainthread_scheduler.hpp
@@ -1,3 +1,5 @@
+// Taken from stdexec inline scheduler and modified accordingly
+
 /*
  * Copyright (c) 2021-2022 NVIDIA Corporation
  *
@@ -35,55 +37,51 @@ QMainWindow* getMainWindow()
 
 namespace stdexec {
 
-  namespace __qtmain {
-    struct __schedule_t
+  namespace qtmain {
+    struct qt_schedule_t
     {
     };
 
-    struct __scheduler
+    struct scheduler
     {
-      using __t = __scheduler;
-      using __id = __scheduler;
+      using t = scheduler;
+      using id = scheduler;
 
-      template <class _Tag = __schedule_t>
+      template <class Tag = qt_schedule_t>
       STDEXEC_ATTRIBUTE((host, device))
-      friend auto tag_invoke(schedule_t, __scheduler)
+      friend auto tag_invoke(schedule_t, scheduler)
       {
-        return __make_sexpr<_Tag>();
+        return __make_sexpr<Tag>();
       }
 
       friend forward_progress_guarantee tag_invoke(
-        get_forward_progress_guarantee_t, __scheduler) noexcept
+        get_forward_progress_guarantee_t, scheduler) noexcept
       {
         return forward_progress_guarantee::weakly_parallel;
       }
 
-      bool operator==(const __scheduler&) const noexcept = default;
+      bool operator==(const scheduler&) const noexcept = default;
     };
-  }    // namespace __qtmain
+  }    // namespace qtmain
 
   template <>
-  struct __sexpr_impl<__qtmain::__schedule_t> : __sexpr_defaults
+  struct __sexpr_impl<qtmain::qt_schedule_t> : __sexpr_defaults
   {
     static constexpr auto get_attrs =    //
       [](__ignore) noexcept
-      -> __env::__prop<__qtmain::__scheduler(get_completion_scheduler_t<set_value_t>)> {
-      return __mkprop(__qtmain::__scheduler{}, get_completion_scheduler<set_value_t>);
+      -> __env::__prop<qtmain::scheduler(get_completion_scheduler_t<set_value_t>)> {
+      return __mkprop(qtmain::scheduler{}, get_completion_scheduler<set_value_t>);
     };
 
     static constexpr auto get_completion_signatures =    //
       [](__ignore, __ignore) noexcept -> completion_signatures<set_value_t()> { return {}; };
 
     static constexpr auto start =    //
-      []<class _Receiver>(__ignore, _Receiver& __rcvr) noexcept -> void {
-      // call set value on the receiver of this sender
-      QCoreApplication* app = QApplication::instance();
-      QApplication* application = static_cast<QApplication*>(QApplication::instance());
+      []<class Receiver>(__ignore, Receiver& rcvr) noexcept -> void {
       QMainWindow* mainwin = getMainWindow();
-
       // call set_value from inside the lambda on the application thread
-      grox::qt::experimental::detail::qt_function_type func = [rcvr = std::move(__rcvr)](bool x) {
-        set_value((_Receiver &&) rcvr);
+      grox::qt::experimental::detail::qt_function_type func = [rcvr = std::move(rcvr)](bool x) {
+        set_value((Receiver &&) rcvr);
         return x;
       };
 
@@ -96,5 +94,5 @@ namespace stdexec {
 
 namespace grox {
   // A scheduler that executes its continuation on the Qt main thread
-  using qt_mainthread_scheduler = stdexec::__qtmain::__scheduler;
+  using qt_mainthread_scheduler = stdexec::qtmain::scheduler;
 }    // namespace grox
