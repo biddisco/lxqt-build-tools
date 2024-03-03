ea8e4ac1cb6f55664b5b090cb6d9643036af9209
John Biddiscombe <biddisco@cscs.ch>
2024-03-03 09:18:37 +0100

    Set/Clear some env vars at startup to fix some Qt locale/session warnings

--- src/main.cpp
+++ src/main.cpp
@@ -456,6 +456,14 @@ void init_resource_partitioner_handler(
 
 int main(int argc, char* argv[])
 {
+  // fix: Qt depends on a UTF-8 locale, and has switched to "C.UTF-8" instead
+  // TODO: Find a real fix
+  setenv("LC_ALL", "C.UTF-8", 1);
+
+  // fix: Qt: Session management error: Could not open network socket
+  // TODO: Find a real fix
+  unsetenv("SESSION_MANAGER");
+  //
   ::argc = argc;
   ::argv = argv;
 
