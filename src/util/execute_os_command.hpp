48862b1353d9580867eb20d82c1f5d31c93a8c96
John Biddiscombe <biddisco@cscs.ch>
2024-03-03 14:31:08 +0100

    Improve os command execute to capture stderr
    Capture the return code of the commmand and display stdout+stderr
    if something goes wrong

--- src/util/execute_os_command.hpp
+++ src/util/execute_os_command.hpp
@@ -5,18 +5,43 @@
 #include <string>
 
 // ----------------------------------------------------------------------------
-inline std::string execute_os_command(const char* cmd)
+// inline std::string execute_os_command(const char* cmd)
+// {
+//   std::array<char, 1024> buffer;
+//   std::string result;
+//   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+//   if (!pipe)
+//   {
+//     throw std::runtime_error("popen() failed!");
+//   }
+//   while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+//   {
+//     result += buffer.data();
+//   }
+//   result.erase(std::remove(result.begin(), result.end(), '\n'), result.end());
+//   return result;
+// }
+
+std::string execute_os_command(const char* cmd)
 {
-  std::array<char, 1024> buffer;
+  std::array<char, 128> buffer;
   std::string result;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  std::string cmd2 = std::string(cmd) + " 2>&1";    // try to capture stderr too
+  auto pipe = popen(cmd2.c_str(), "r");             // get rid of shared_ptr
   if (!pipe)
-  {
     throw std::runtime_error("popen() failed!");
-  }
-  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+  while (!feof(pipe))
   {
-    result += buffer.data();
+    if (fgets(buffer.data(), buffer.size(), pipe) != nullptr)
+      result += buffer.data();
+  }
+  auto rc = pclose(pipe);
+  if (rc != EXIT_SUCCESS)
+  {
+    std::cout << "OS Command : " << std::endl
+              << cmd2 << std::endl
+              << "failed with code " << rc << std::endl
+              << result << std::endl;
   }
   result.erase(std::remove(result.begin(), result.end(), '\n'), result.end());
   return result;
