11462f0aa68e28170bb8e44e76b001a585d1457d
John Biddiscombe <biddisco@cscs.ch>
2024-06-01 01:21:44 +0200

    Update python h5py copy script to be able to convert float64 to float32 datasets

--- scripts/h5copy.py
+++ scripts/h5copy.py
@@ -1,10 +1,24 @@
 import h5py
 
-infile = '/home/biddisco/.local/share/grox/grox.hdf5'
+def copy_datasets(group, new_group):
+    for name, obj in group.items():
+        if isinstance(obj, h5py.Dataset):
+            print(name, obj.dtype)
+            if obj.dtype == 'float64':
+                print(f'Making float32 copy of dataset: {name}')
+                new_group.create_dataset(name, shape=obj.shape, dtype='float32', data=obj[:])
+            else:
+                print(f'Copying directly dataset: {name}')
+                new_group.create_dataset(name, shape=obj.shape, dtype=obj.dtype, data=obj[:])
+        elif isinstance(obj, h5py.Group):
+            print('Creating group', name)
+            new_subgroup = new_group.create_group(name)
+            copy_datasets(obj, new_subgroup)
 
+infile = '/home/biddisco/.local/share/grox/grox.hdf5'
 h5r=h5py.File(infile, 'r')
 
-with h5py.File("f2.h5", 'w') as h5w:
-    for obj in h5r.keys():
-        h5r.copy(obj, h5w )
+with h5py.File("new_file.h5", 'w') as h5w:        
+    copy_datasets(h5r, h5w)
+
 h5r.close()
