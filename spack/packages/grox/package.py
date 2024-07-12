4e07279e171b7a58f254d397a84e64b04f3b5780
John Biddiscombe <biddisco@cscs.ch>
2024-07-12 07:53:02 +0200

    latest version of spack package for grox, using home envdev package usually

--- spack/packages/grox/package.py
+++ spack/packages/grox/package.py
@@ -2,7 +2,7 @@ from spack import *
 import os
 
 class Grox(BundlePackage, CudaPackage, ROCmPackage):
-    """dummy placeholder for oomph dependencies"""
+    """dummy placeholder"""
     homepage    = "https://127.0.0.1/readme.html"
     git = "pi:~/gitrepos/grox"
     generator   = "Ninja"
@@ -24,12 +24,13 @@ class Grox(BundlePackage, CudaPackage, ROCmPackage):
     variant("apex", default=False, description="Turn on APEX support")
 
     # ------------------------------------------------------------------------
-    # build time dependencies
+    # build tools and support
     # ------------------------------------------------------------------------
-    depends_on("ninja", type="build")
+    depends_on("ninja",       type="build")
     depends_on("cmake@3.23:", type="build")
-    depends_on("iwyu", type="build", when="+iwyu")
-    depends_on("llvm@17", when="+llvm")
+    depends_on("iwyu",        type="build", when="+iwyu")
+    depends_on("llvm@16", when="+llvm")
+    depends_on('clang-format@16')
 
     # ------------------------------------------------------------------------
     # generic c++ libs
@@ -37,13 +38,7 @@ class Grox(BundlePackage, CudaPackage, ROCmPackage):
     depends_on("boost +atomic+chrono+container+context+coroutine+date_time+filesystem+program_options+regex+serialization+system+test+thread+json cxxstd=17")
     depends_on("stdexec@main")
     depends_on("fmt")
-
-    depends_on("hdf5 +hl")
-
-    # ------------------------------------------------------------------------
-    # pika depends on hwloc
-    # ------------------------------------------------------------------------
-    depends_on("hwloc") 
+    depends_on("hdf5 +hl +cxx")
     # if pika was compiled with mpi ...
     depends_on("mpi", when="+mpi")
 
@@ -55,17 +50,22 @@ class Grox(BundlePackage, CudaPackage, ROCmPackage):
     depends_on("whip",       when="+cuda")
 
     # ------------------------------------------------------------------------
-    # allocators/memory
+    # allocators/memory/system
     # ------------------------------------------------------------------------
+    depends_on("hwloc")
     depends_on("mimalloc")
     depends_on("umpire")
     depends_on("umpire +cuda", when="+cuda")
 
+    depends_on("openblas")
+
     # ------------------------------------------------------------------------
+    # logging
     # profiling
     # ------------------------------------------------------------------------
+    depends_on("spdlog")
     depends_on("apex@develop +papi +gperftools +binutils ~activeharmony ~cuda ~examples ~hip ~ipo ~jemalloc ~kokkos ~lmsensors ~mpi ~openmp ~otf2 ~plugins ~starpu ~sycl ~tests build_type=Debug dev_path=/home/biddisco/src/apex", when="+apex")
-    depends_on("otf2") 
+    depends_on("otf2, when="+apex"") 
 
     # ------------------------------------------------------------------------
     # testing
@@ -75,7 +75,6 @@ class Grox(BundlePackage, CudaPackage, ROCmPackage):
     # ------------------------------------------------------------------------
     # rippled dependencies
     # ------------------------------------------------------------------------
-    depends_on("abseil-cpp@20230802.1") # later versions have grpc/abseil compilation problems
     depends_on("soci@master cxxstd=20 +sqlite ~static +boost ~visibility")
     depends_on("libarchive")
     depends_on("lz4")
@@ -86,6 +85,7 @@ class Grox(BundlePackage, CudaPackage, ROCmPackage):
     depends_on("date")
     depends_on("NuDB")
     depends_on("re2")
+    depends_on("xxhash")
 
     # ------------------------------------------------------------------------
     # ------------------------------------------------------------------------
