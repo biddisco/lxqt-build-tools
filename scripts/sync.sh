e25dace86bd0515e4703c0037592318ff9e55524
John Biddiscombe <biddisco@cscs.ch>
2024-09-17 12:45:54 +0200

    Sync/backup script updated for spare laptop use

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,22 +1,23 @@
+#!/bin/bash
 #params="--delete"
 params="--exclude '*.pack'"
 
 echo "-----------------------------------"
 echo "Updating to Raspberry Pi"
 echo "-----------------------------------"
-$HOME/update-rsync.sh $HOME/src/grox/ pi:/home/pi/src/grox $params
+$HOME/update-rsync.sh $HOME/src/grox/ pi5:/home/pi/src/grox $params
 
 echo
 echo "-----------------------------------"
-echo "Updating to old laptop (cable)"
+echo "Updating to old laptop (cable ip address)"
 echo "-----------------------------------"
-# RJ45 network cable
 $HOME/update-rsync.sh $HOME/src/grox/ oryx:/home/biddisco/src/grox $params
+$HOME/update-rsync.sh $HOME/src/grox/  pop:/home/biddisco/src/grox $params
 
-echo
-echo "-----------------------------------"
-echo "Updating to old laptop (wifi)"
-echo "-----------------------------------"
-# Wifi connection
-$HOME/update-rsync.sh $HOME/src/xrp/grox/ biddisco@192.168.1.112:/home/biddisco/src/grox $params
-
+if [[ "$(hostname)" == "elf" ]]; then
+    $HOME/update-rsync.sh /home/biddisco/.config/grox.ini            pop:/home/biddisco/.config/grox.ini
+    $HOME/update-rsync.sh /home/biddisco/.local/share/grox/grox.hdf5 pop:/home/biddisco/.local/share/grox/grox.hdf5
+else
+    $HOME/update-rsync.sh /home/biddisco/.config/grox.ini            elf:/home/biddisco/.config/grox.ini
+    $HOME/update-rsync.sh /home/biddisco/.local/share/grox/grox.hdf5 elf:/home/biddisco/.local/share/grox/grox.hdf5
+fi
