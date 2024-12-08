6dcdd15a658eb3609d19e8c1428317f975d6b18c
John Biddiscombe <biddisco@cscs.ch>
2024-12-08 15:05:29 +0100

    Move qrc file containing images into widgets lib

--- src/apps/grox/main.cpp
+++ src/apps/grox/main.cpp
@@ -142,6 +142,7 @@ int qt_main(pika::program_options::variables_map& vm)
   QApplication app(argc, argv);
   QNetworkAccessManager networkmanager;
   //
+  Q_INIT_RESOURCE(images);
   QIcon icon(":images/icons/xrp-logo-white-black.svg");
   app.setWindowIcon(icon);
   app.setApplicationName("grox");
