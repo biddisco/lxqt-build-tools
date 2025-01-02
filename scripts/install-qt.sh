851b2187915a10c6353c70affac16f36d64c853f
John Biddiscombe <biddisco@cscs.ch>
2025-01-02 12:56:36 +0100

    Add Qt installation script (using extern qt install repos)

---
+++ scripts/install-qt.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# this is the directory of the script, regardless of where it's called from
+if [[ $0 != $BASH_SOURCE ]]; then
+  # this script was sourced from somewhere, expend name if a symlink
+  SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE))
+else
+  # this was executed directly
+  SCRIPT_DIR="$(readlink -f $(dirname $0))"
+fi
+
+# https://github.com/miurahr/aqtinstall
+
+export AQT_VERSION="aqtinstall==3.1.21"
+export QT_VERSION=6.8.1
+export QT_PATH=/opt/Qt
+export QT_GCC=${QT_PATH}/${QT_VERSION}/gcc_64 \
+
+# create a virtual env
+python3 -m venv aqt-env
+source aqt-env/bin/activate
+
+pip install "$AQT_VERSION"
+
+# display all Qt6 modulkes available
+aqt list-qt linux desktop --long-modules 6.8.1 linux_gcc_64
+
+# install "all" qt modules
+sudo aqt-env/bin/aqt install-qt -O "$QT_PATH" linux desktop "$QT_VERSION" linux_gcc_64 -m all
+
+deactivate
+
+#sudo aqt install-tool -O "$QT_PATH" linux desktop tools_cmake
+#sudo aqt install-tool -O "$QT_PATH" linux desktop tools_ninja
+
