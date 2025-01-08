6e72773c2a246731c6290a4c0f87406529549d91
John Biddiscombe <biddisco@cscs.ch>
2025-01-08 15:52:26 +0100

    Add some tests for util datetime conversions

--- src/util/datetime_utils.hpp
+++ src/util/datetime_utils.hpp
@@ -1,9 +1,44 @@
 #pragma once
 
+#include <chrono>
+#include <iomanip>
 #include <string>
 //
 #include <QDateTime>
 #include <QLocale>
+//
+
+// ----------------------------------------------------------------------------
+// namespace grox::date {
+
+//   std::chrono::system_clock::time_point datetime(std::string const& s)
+//   {
+//     std::istringstream in{s};
+//     std::chrono::year y;
+//     std::chrono::month_day md;
+//     if (in.peek() == 'X')
+//     {
+//       in >> std::chrono::parse("XXXX-%m-%d", md);
+//       if (in.fail())
+//         throw std::runtime_error(
+//             "Unable to parse a date of the form XXXX-mm-dd out of \"" + s + '"');
+//       y = std::chrono::year_month_day{std::chrono::floor<std::chrono::days>(
+//                                           std::chrono::system_clock::now())}
+//               .year();
+//     }
+//     else
+//     {
+//       in >> std::chrono::parse("%Y-", y) >> std::chrono::parse("%m-%d", md);
+//       if (in.fail())
+//         throw std::runtime_error(
+//             "Unable to parse a date of the form yyyy-mm-dd out of \"" + s + '"');
+//     }
+//     auto date = y / md;
+//     if (!date.ok()) throw std::runtime_error("Parsed invalid date out of \"" + s + '"');
+//     return std::chrono::sys_days{date};
+//   }
+
+// }    // namespace grox::date
 
 // ----------------------------------------------------------------------------
 // unixtime * 1000 is msecs since 1970/1/1
---
+++ src/util/test/datetime.cpp
@@ -0,0 +1,89 @@
+#include <bitset>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <string>
+//
+#include <QString>
+//
+#include <fmt/format.h>
+//
+#include "util/datetime_utils.hpp"
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv);
+
+// ------------------------------------------------------------------
+std::string diff(std::string const& s1, std::string const& s2)
+{
+  std::stringstream tmp;
+  for (std::size_t index = 0; index < s1.size(); index++)
+  {
+    if (index < s2.size())
+    {
+      if (s1[index] == s2[index])
+        tmp << (s1[index] == '\n' ? "\n" : ".");
+      else
+        tmp << s1[index];
+    }
+  }
+  tmp << std::endl;
+  for (std::size_t index = 0; index < s2.size(); index++)
+  {
+    if (index < s1.size())
+    {
+      if (s1[index] == s2[index])
+        tmp << (s2[index] == '\n' ? "\n" : ".");
+      else
+        tmp << s2[index];
+    }
+  }
+  return tmp.str();
+}
+
+// ------------------------------------------------------------------
+bool compare(std::string const& expected, std::string const& received)
+{
+  if (bool ok = (expected == received))
+  {
+    std::cout << "passed : \"" << expected << "\"" << std::endl;
+    return ok;
+  }
+  else
+  {
+    // clang-format off
+    std::cout
+        << "expected : " << expected.size() << "\n" << expected << "\n"
+        << "received : " << received.size() << "\n" << received << "\n"
+        << diff(expected, received) << std::endl;
+    // clang-format on
+    return ok;
+  }
+}
+
+// ------------------------------------------------------------------
+TEST(util, datetime)
+{
+  {
+    std::uint64_t msec =
+        std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1);
+    msec = 1736347128670;
+    std::string tstr = msecs_unix_to_calendar_time(msec);
+    EXPECT_TRUE(compare("2025-01-08 15:38:48", tstr));
+  }
+  {
+    std::uint64_t sec =
+        std::chrono::system_clock::now().time_since_epoch() / std::chrono::seconds(1);
+    sec = 1736347437;
+    std::string tstr = secs_unix_to_calendar_time(sec);
+    EXPECT_TRUE(compare("2025-01-08 15:43:57", tstr));
+  }
+}
+
+// ------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
