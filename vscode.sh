6c34b3820263ba14670c376ec4dda30e18a8ffd3
John Biddiscombe <biddisco@cscs.ch>
2026-02-21 21:51:44 +0100

    vscode: add files for vscode for c++ as well as python

---
+++ vscode.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# this is the directory of the script, regardless of where it's called from
+if [[ $0 != $BASH_SOURCE ]]; then
+  # this script was sourced from somewhere, expand name if a symlink
+  SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE))
+else
+  # this was executed directly
+  SCRIPT_DIR="$(readlink -f $(dirname $0))"
+fi
+echo script dir is $SCRIPT_DIR
+
+# activate python env if it exists
+if [[ -f "$SCRIPT_DIR/python/.venv/bin/activate" ]]; then
+  echo Activating python environment in $SCRIPT_DIR
+  source "$SCRIPT_DIR/python/.venv/bin/activate"
+else
+  source /home/biddisco/benchmarking-results/.venv/bin/activate
+fi
+
+echo checking for opal environment
+#if spack env list | grep -q 'opal'; then
+  echo Activating spack environment in $SCRIPT_DIR
+  spack env activate --prompt opal
+#fi
+
+echo Launching vscode in $SCRIPT_DIR
+# launch vscode in our dir
+code $SCRIPT_DIR
